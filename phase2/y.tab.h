8e072980ce680aab34d5f3f1cbaacd6c917b739e
Hema-Sri-Ch <hemasrircpm@gmail.com>
2023-11-10 20:31:09 +0530

    added some semantics

---
+++ phase2/y.tab.h
@@ -0,0 +1,188 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    newid = 258,                   /* newid  */
+    INT_CONST = 259,               /* INT_CONST  */
+    FLOAT_CONST = 260,             /* FLOAT_CONST  */
+    CHAR_CONST = 261,              /* CHAR_CONST  */
+    STR_CONST = 262,               /* STR_CONST  */
+    BOOL_CONST = 263,              /* BOOL_CONST  */
+    ARROW = 264,                   /* ARROW  */
+    PUNC = 265,                    /* PUNC  */
+    DATATYPE = 266,                /* DATATYPE  */
+    VOID = 267,                    /* VOID  */
+    RETURN = 268,                  /* RETURN  */
+    IF = 269,                      /* IF  */
+    ELSE = 270,                    /* ELSE  */
+    CONTINUE = 271,                /* CONTINUE  */
+    BREAK = 272,                   /* BREAK  */
+    MATRIX = 273,                  /* MATRIX  */
+    GRAPH = 274,                   /* GRAPH  */
+    VECT = 275,                    /* VECT  */
+    SWITCH = 276,                  /* SWITCH  */
+    CASE = 277,                    /* CASE  */
+    DEFAULT = 278,                 /* DEFAULT  */
+    CLASS = 279,                   /* CLASS  */
+    STRUCT = 280,                  /* STRUCT  */
+    LOGOP = 281,                   /* LOGOP  */
+    ARITHOP = 282,                 /* ARITHOP  */
+    NOT = 283,                     /* NOT  */
+    UNARYOP = 284,                 /* UNARYOP  */
+    DECLR = 285,                   /* DECLR  */
+    EXPR = 286,                    /* EXPR  */
+    CALL = 287,                    /* CALL  */
+    FUNC = 288,                    /* FUNC  */
+    LOOP = 289,                    /* LOOP  */
+    null = 290,                    /* null  */
+    FOR = 291,                     /* FOR  */
+    WHILE = 292,                   /* WHILE  */
+    APPEND = 293,                  /* APPEND  */
+    REMOVE = 294,                  /* REMOVE  */
+    LENGTH = 295,                  /* LENGTH  */
+    SORT = 296,                    /* SORT  */
+    CLEAR = 297,                   /* CLEAR  */
+    AT = 298,                      /* AT  */
+    TRANSPOSE = 299,               /* TRANSPOSE  */
+    TRACE = 300,                   /* TRACE  */
+    TRAVERSAL = 301,               /* TRAVERSAL  */
+    STRLEN = 302,                  /* STRLEN  */
+    STRCUT = 303,                  /* STRCUT  */
+    STRCMP = 304,                  /* STRCMP  */
+    STRJOIN = 305,                 /* STRJOIN  */
+    MATXOP = 306,                  /* MATXOP  */
+    MAXTOGR = 307,                 /* MAXTOGR  */
+    GRTOMATX = 308,                /* GRTOMATX  */
+    SHPATH = 309,                  /* SHPATH  */
+    SHPATHVAL = 310,               /* SHPATHVAL  */
+    GOTO = 311                     /* GOTO  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+/* Token kinds.  */
+#define YYEMPTY -2
+#define YYEOF 0
+#define YYerror 256
+#define YYUNDEF 257
+#define newid 258
+#define INT_CONST 259
+#define FLOAT_CONST 260
+#define CHAR_CONST 261
+#define STR_CONST 262
+#define BOOL_CONST 263
+#define ARROW 264
+#define PUNC 265
+#define DATATYPE 266
+#define VOID 267
+#define RETURN 268
+#define IF 269
+#define ELSE 270
+#define CONTINUE 271
+#define BREAK 272
+#define MATRIX 273
+#define GRAPH 274
+#define VECT 275
+#define SWITCH 276
+#define CASE 277
+#define DEFAULT 278
+#define CLASS 279
+#define STRUCT 280
+#define LOGOP 281
+#define ARITHOP 282
+#define NOT 283
+#define UNARYOP 284
+#define DECLR 285
+#define EXPR 286
+#define CALL 287
+#define FUNC 288
+#define LOOP 289
+#define null 290
+#define FOR 291
+#define WHILE 292
+#define APPEND 293
+#define REMOVE 294
+#define LENGTH 295
+#define SORT 296
+#define CLEAR 297
+#define AT 298
+#define TRANSPOSE 299
+#define TRACE 300
+#define TRAVERSAL 301
+#define STRLEN 302
+#define STRCUT 303
+#define STRCMP 304
+#define STRJOIN 305
+#define MATXOP 306
+#define MAXTOGR 307
+#define GRTOMATX 308
+#define SHPATH 309
+#define SHPATHVAL 310
+#define GOTO 311
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
